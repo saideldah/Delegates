c80642cfc26bfec87ba224eaf78a5a9b3a8f0139
Said El Dah <saeed.eldah@gmail.com>
2016-09-11 17:10:16 +0300

    Delegate Example Done

---
+++ Delegates/PhotoProcessor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class PhotoProcessor
+    {
+        public delegate void FilterDelegate(Photo photo);
+        public static void ApplyFilter(Photo photo, Action<Photo> someFilter)
+        {
+            someFilter(photo);
+        }
+
+        public static void ApplyFilter2(Photo photo, FilterDelegate filterDelegate)
+        {
+            filterDelegate(photo);
+        }
+    }
+}
--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -10,7 +10,28 @@ namespace Delegates
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Photo myPhoto = new Photo();
+            PhotoProcessor.ApplyFilter(myPhoto, Blur);
+
+            Action<Photo> filterDelegate = RedEyeRemoval;
+            filterDelegate += BlackAndWhit;
+            PhotoProcessor.ApplyFilter(myPhoto, filterDelegate);
+
+            PhotoProcessor.ApplyFilter2(myPhoto, Blur);
+
+        }
+        static void RedEyeRemoval(Photo photo)
+        {
+            Console.WriteLine("Red Eye Removal Filter");
+        }
+
+        static void Blur(Photo photo)
+        {
+            Console.WriteLine("Blur Filter");
+        }
+        static void BlackAndWhit(Photo photo)
+        {
+            Console.WriteLine("Black And Whit Filter");
         }
     }
 }
